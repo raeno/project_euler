efc739a8b060225e8473d106743614a54ae5de0a
raeno <just.raeno@gmail.com>
2013-04-20 13:31:19 +0400

    Started working on method to convert number in its words representation. Added conversion digits to words.

--- tools/numbers.rb
+++ tools/numbers.rb
@@ -1,4 +1,7 @@
 class Fixnum
+  DIGITS_IN_WORDS = { 0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four',
+                      5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine' }
+
   def palindrome?
     num_string = self.to_s
     left_border = num_string.length/2
@@ -6,7 +9,19 @@ class Fixnum
     num_string[0..left_border-1] == num_string[right_border..num_string.length].reverse
   end
 
-   def factorial
+  def factorial
  	(2..self).inject(1) { |mem, var| mem *= var  }
- end
+  end
+
+  def in_words
+  	if self >= 0 && self < 10
+  		return DIGITS_IN_WORDS[self]
+    end
+
+  end
+
+  
 end
+
+
+p 9.in_words  
