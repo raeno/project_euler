b165b1669ea4029b6161d60222ed0b7a775bed06
raeno <just.raeno@gmail.com>
2013-04-20 13:44:53 +0400

    Added conversion number to words up to 100

--- tools/numbers.rb
+++ tools/numbers.rb
@@ -1,6 +1,10 @@
 class Fixnum
   DIGITS_IN_WORDS = { 0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four',
                       5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine' }
+  FIRST_DECADE_IN_WORDS = { 10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 
+                            15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen'}
+  DECADES_IN_WORDS = { 20 => 'twenty', 30 => 'thirty', 40 => 'fourty', 50 => 'fifty', 60 => 'sixty',
+                       70 => 'seventy', 80 => 'eighty', 90 => 'ninety'} 
 
   def palindrome?
     num_string = self.to_s
@@ -14,14 +18,24 @@ class Fixnum
   end
 
   def in_words
-  	if self >= 0 && self < 10
-  		return DIGITS_IN_WORDS[self]
+  	case self
+    when 0..9
+      DIGITS_IN_WORDS[self]
+    when 10..19
+      FIRST_DECADE_IN_WORDS[self]
+    when 20..99
+      if self % 10 == 0
+        DECADES_IN_WORDS[self]
+      else
+        last_digit = self % 10
+        decade = self - last_digit
+        DECADES_IN_WORDS[decade] + '-' + DIGITS_IN_WORDS[last_digit]
+      end
     end
-
   end
 
   
 end
 
 
-p 9.in_words  
+p 79.in_words  
