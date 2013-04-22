c736eccf77278bf8133b58dec652bf0f66607a4b
raeno <just.raeno@gmail.com>
2013-04-22 11:50:08 +0400

    Solutions for problems 6-8 reformatted in a test_case style.

--- 006.rb
+++ 006.rb
@@ -5,5 +5,14 @@
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-p ((1..100).reduce(&:+)) ** 2 - (1..100).map { |x| x**2 }.reduce(&:+)
+require "test/unit"
 
+class TestProblem_6 < Test::Unit::TestCase
+	def test_difference_between_sum_of_squares_and_square_of_sum
+		sum_of_squares = (1..100).map { |x| x**2 }.reduce(&:+)
+		square_of_sum = (1..100).reduce(&:+) ** 2
+		
+		difference = sum_of_squares - square_of_sum
+		assert_equal(25164150, difference.abs)		
+	end
+end
--- 007.rb
+++ 007.rb
@@ -1,5 +1,11 @@
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 # What is the 10 001st prime number?
 
+require "test/unit"
 require_relative 'tools/prime_numbers'
-p eratosthenes(10**6)[10000]
+
+class TestProblem_7 < Test::Unit::TestCase
+	def test_10001st_prime_number
+		assert_equal(104743, eratosthenes(10**6)[10000])
+	end
+end
