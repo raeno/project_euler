a67a5e2ba5eea502a365805e4a3be0aadac7fd52
raeno <just.raeno@gmail.com>
2013-04-22 19:06:15 +0400

    Solver problem #22.

---
+++ 022.rb
@@ -0,0 +1,34 @@
+# Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand
+# first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name,
+# multiply this value by its alphabetical position in the list to obtain a name score.
+# For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, 
+# is the 938th name in the list. So, COLIN would obtain a score of 938  53 = 49714.
+
+# What is the total of all the name scores in the file?
+
+require 'test/unit'
+
+
+class TestProblem_22 < Test::Unit::TestCase
+
+  def letters_values
+    @letters ||= Hash[*('A'..'Z').to_a.each_with_index.map { |v,i| [v,i+1] }.flatten]
+  end
+
+  def total_score
+    all_words = []
+    File.open('data/problem_22_names.txt') do |file|
+      all_words = file.readline.split(',').sort.map { |str| str.gsub /"/, '' }
+    end
+
+    scores =  all_words.map { |str| str.chars.reduce(0) { |mem, ch| mem += letters_values[ch]} }
+    final_scores = scores.each_with_index.map { |v,i| v * (i +1) }
+
+    final_scores.reduce(&:+)
+  end
+
+
+	def test_sum_of_all_name_scores
+    assert_equal 871198282, total_score
+	end
+end
