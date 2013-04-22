39425c5c63797ab29130a787cb199b02400a23ea
raeno <just.raeno@gmail.com>
2013-04-22 14:26:10 +0400

    Problem 14 solution reformatted in a test_case style. Also refactored a little.

--- 014.rb
+++ 014.rb
@@ -1,4 +1,7 @@
-class Collatzchain
+require 'test/unit'
+
+
+class CollatzChain
 	attr_reader :chain
 	
 	def initialize(number)
@@ -16,18 +19,24 @@ class Collatzchain
 
 	def next_number(number)
 		if number.even?
-			number = number/2
+			number/2
 		else
-			number = 3*number + 1
+			3*number + 1
+		end
+	end
+
+	class << self
+		def longest_chain(max_start_num)
+			chains_lengths = (1..max_start_num).map { |n| [n,CollatzChain.new(n).length] }
+			chains_lengths = Hash[chains_lengths]
+			chains_lengths.max_by { |_,v| v}
 		end
 	end
 end
 
-def longest_collatzChain(max_start_num)
-	chains_lengths = (1..max_start_num).map { |n| [n,Collatzchain.new(n).length] }
-	chains_lengths = Hash[chains_lengths]
-	chains_lengths.max_by { |k,v| v}
 
+class TestProblem_14 < Test::Unit::TestCase
+	def test_longest_collatz_chain
+		assert_equal [837799,524], CollatzChain.longest_chain(10**6)
+	end
 end
-
-p longest_collatzChain(10**6)
