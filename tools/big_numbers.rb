5429a05c0a3e35a8e03548c73afba83d1b9c1614
raeno <just.raeno@gmail.com>
2013-05-11 19:51:20 +0400

    Solved problem #36. Not checked on website yet. Added utilities method to numbers to find base2 palindrome. Also changed palinrome method behaviour, now it's return true for numbers from (0..9). Added palimdrome? method for BigNumber's.

--- tools/big_numbers.rb
+++ tools/big_numbers.rb
@@ -3,4 +3,11 @@ class Bignum
     digits = self.to_s.chars.map(&:to_i)
     digits.inject { |sum, var| sum += var  }
   end
+
+  def palindrome?
+    num_string = self.to_s
+    left_border = num_string.length/2
+    right_border = num_string.length.even? ? left_border : left_border + 1
+    num_string[0..left_border-1] == num_string[right_border..num_string.length].reverse
+  end
 end
--- tools/numbers.rb
+++ tools/numbers.rb
@@ -22,9 +22,16 @@ class Fixnum
 
   def palindrome?
     num_string = self.to_s
+    return true if num_string.length == 1
+
     left_border = num_string.length/2
     right_border = num_string.length.even? ? left_border : left_border + 1
     num_string[0..left_border-1] == num_string[right_border..num_string.length].reverse
+  end
+
+  def base2_palindrome?
+    binary = self.to_s(2).to_i
+    binary.palindrome?
   end
 
   def divisors_powers
