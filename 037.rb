7319626d73e4a937ab603ef40288bb8d5c00cf14
raeno <just.raeno@gmail.com>
2013-05-12 14:22:53 +0400

    Solved problem #27.

--- 037.rb
+++ 037.rb
@@ -2,17 +2,13 @@ require "test/unit"
 require_relative 'tools/numbers'
 
 class Test_Problem37 < Test::Unit::TestCase
-	def test_sum_of_eleven_truncatable_primes
-
-		#Fixnum.primes = eratosthenes 10**7
-
-		primes = primes_without_even_digits
+	def find_truncatable_primes
+		primes = Fixnum.primes.to_a
 		[2,3,5,7].each { |v| primes.delete v }
+		primes.select { |p| p.truncatable_prime? }
+	end
 
-		primes = primes.select { |p| p.truncatable_prime? }
-
-		p primes
-		p primes.count
-		
+	def test_sum_of_eleven_truncatable_primes
+		assert_equal 100, find_truncatable_primes.reduce(0) {|mem,var| mem += var }
 	end
 end
