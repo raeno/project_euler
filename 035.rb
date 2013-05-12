3c611dd313a750e0c1419b585bd2c28d8be9bc9a
raeno <just.raeno@gmail.com>
2013-05-12 10:01:13 +0400

    Optimized solution for problem #35. Removed all primes with at least one even digit from calculation ( one of it's permutations became even number and so no prime). Time decreased to ~5 seconds from 2 minutes.

--- 035.rb
+++ 035.rb
@@ -3,8 +3,15 @@ require "test/unit"
 require_relative 'tools/numbers'
 
 class Test_Problem35 < Test::Unit::TestCase
-	def test_circular_primes_below_one_million
+
+	def primes_without_even_digits
 		primes = Fixnum.primes
+		even_chars = [0,2,4,6,8].map(&:to_s)
+		primes = primes.select { |p| !(p.to_s.chars & even_chars).any? }
+	end
+
+	def test_circular_primes_below_one_million
+		primes = primes_without_even_digits << 2
 		circular_primes =  primes.select { |prime| prime.circular_prime? }
 		assert_equal 22, circular_primes.count
 	end
