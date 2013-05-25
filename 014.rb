78cdd46ea90a8cf292b0425fe0d6968d408a3bce
raeno <just.raeno@gmail.com>
2013-05-25 23:37:36 +0400

    Optimized longest Collatz chain, now it keep in memory lengths of previous chains and check it with every next number. Also removed overhead with CollatzChain class, all this instance creating stuff also slowed test. Now it runs 2,3 seconds on my PC. Previous version runs 14 seconds.

--- 014.rb
+++ 014.rb
@@ -1,42 +1,32 @@
 require 'test/unit'
-
-
-class CollatzChain
-	attr_reader :chain
-	
-	def initialize(number)
-		current = number
-		@chain = []
-		until current == 1
-			current = next_number(current)
-			@chain << current	
-		end
-	end
-
-	def length 
-		@chain.length
-	end
-
-	def next_number(number)
-		if number.even?
-			number/2
-		else
-			3*number + 1
-		end
-	end
-
-	class << self
-		def longest_chain(max_start_num)
-			chains_lengths = (1..max_start_num).map { |n| [n,CollatzChain.new(n).length] }
-			chains_lengths = Hash[chains_lengths]
-			chains_lengths.max_by { |_,v| v}
-		end
-	end
-end
-
+require 'benchmark'
 
 class TestProblem_14 < Test::Unit::TestCase
+
+	def chain_length(number)
+		length = 0
+		current = number
+		
+		until current == 1
+			if @lengths[current]
+				length += @lengths[current]
+				break
+			end
+			current = current.even? ? current/2 : 3*current + 1
+			length += 1
+		end
+		@lengths[number] = length
+	end
+
+	def find_longest_length(limit)
+		@lengths = {}
+		(2..limit).each do |start_number|
+			chain_length(start_number)
+		end
+		@lengths.max_by { |k,v| v }
+	end
+
 	def test_longest_collatz_chain
-		assert_equal [837799,524], CollatzChain.longest_chain(10**6)
+		assert_equal [837799,524], find_longest_length(10**6)
 	end
 end
