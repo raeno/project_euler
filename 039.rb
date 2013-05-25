d34204e9cb718e332e9e3c607ece338ac88ca491
raeno <just.raeno@gmail.com>
2013-05-25 22:36:20 +0400

    Changed algorithm for right triangle check. Now it's O(n^2) instead of O(n^3) Total time also reduced from ~100 seconds to 0.16 s :)

--- 039.rb
+++ 039.rb
@@ -1,30 +1,23 @@
 require "test/unit"
-
 require 'set'
 
 class Test_Problem39 < Test::Unit::TestCase
 
-	def correct_triangle(a,b,c)
-		(a + b > c) && (a +c > b) && (b + c > a)
-	end
-
-	def right_angle_triangle(a,b,c)
-		return false if a == b || a == c || b == c
-		(a*a + b*b == c*c) || (a*a + c*c == b*b)|| (b*b + c*c == a*a)
+	# sqrt(a^2+b^2) is an integer for right triangle
+	def sum_square(a,b)
+		x = Math.sqrt(a*a + b*b)
+		(x - x.to_i).abs < 0.001 ? x.to_i : false
 	end
 
 	def get_triangles
 		triangles = Hash.new { |h,k| h[k] = Set.new }
 
-		(1..100).each do |perimeter|
-			(1..998).each do |a|
-				(1..999-a).each do |b|
-					c = perimeter - a - b
-
-					triangles[perimeter] << [a,b,c].sort if correct_triangle(a,b,c) && right_angle_triangle(a,b,c)
-				end
+		(1..998).each do |a|
+			(a..999-a).each do |b|
+				c = sum_square(a,b)
+				triangles[a+b+c] << [a,b,c].sort if c && a+b+c <= 1000
 			end
-		end
+		end	
 		triangles
 	end
 
