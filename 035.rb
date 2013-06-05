e9c1948758d8b65190b4dab3a3fd34dae30975c1
raeno <just.raeno@gmail.com>
2013-06-05 20:22:12 +0400

    Removed a debug output for #35 problem.

--- 035.rb
+++ 035.rb
@@ -8,7 +8,6 @@ class Test_Problem35 < Test::Unit::TestCase
 		primes = Fixnum.primes.to_a
 		primes = primes_without_even_digits << 2
 		circular_primes =  primes.select { |prime| prime.circular_prime? }
-		p circular_primes
 		assert_equal 55, circular_primes.count
 	end
 end
