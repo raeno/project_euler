d7fbea4b59255cd29fc5d5bfc73e512a1120ae15
raeno <just.raeno@gmail.com>
2015-06-02 22:15:41 +0300

    Solved a problem #57

---
+++ 057.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+
+class Test_Problem57 < Test::Unit::TestCase
+
+	def fractions_sequence(limit)
+		items = [ [3,2] ]
+		(2..limit).each do |index|
+			numerator, denominator = items[index - 2]
+			items << [ numerator + denominator * 2, numerator + denominator ]
+		end
+		items
+	end
+
+	def test_numerator_count
+		fractions_count = fractions_sequence(1000).select { |item| item[0].to_s.length > item[1].to_s.length }.count
+		assert_equal 153, fractions_count
+	end
+end
+
