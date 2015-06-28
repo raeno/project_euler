d186b04f727d95b549e6233b81b1052d45b2a256
raeno <just.raeno@gmail.com>
2015-06-28 12:23:27 +0300

    Problem 1. Add pure mathematical solution without iterating which works for O(1). Add performance tests.

--- 000_050/001.rb
+++ 000_050/001.rb
@@ -4,11 +4,40 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 require 'test/unit'
+require "minitest/benchmark" 
 
 class TestProblem_1 < Test::Unit::TestCase
 
+	def find_all_multiples(limit)
+		(1..limit-1).select { |n| n % 3 == 0  or n % 5 == 0}.reduce(:+)
+	end
+
+	def find_mathematically(limit)
+	 	three, five, fifteen =  [3,5,15].map do |num|
+	 		count = (limit - 1) / num
+	 		num * count * (count +1) / 2
+	 	end
+	 	three + five - fifteen
+	end
+
+
 	def test_sum_of_all_multiples_of_3_and_5
-		sum = (1..999).select { |n| n % 3 == 0  or n % 5 == 0}.reduce(:+)
+		sum = find_all_multiples(1000)
 		assert_equal 233168, sum
+	end
+
+	def test_mathematical_solution
+		sum = find_mathematically 1000
+		assert_equal 233168, sum
+	end
+
+	def test_performance
+		 assert_performance_linear 0.9999 do |n| # n is a range value
+      		100.times { find_all_multiples n }
+    	end
+
+    	assert_performance_constant 0.9999 do |n|
+    		100.times { find_mathematically n }
+    	end
 	end
 end
